df6f492304bea0162b8d6e97fd94d7e8dc7a917c
quentinM22 <quentin.meunier1@laposte.net>
2022-11-29 17:05:54 +0100

    feat: Gestion Des Erreur

--- starterOnly/js/main.js
+++ starterOnly/js/main.js
@@ -1,14 +1,155 @@
-console.log("Script Chargé ...");
-
-/* Modal Close */
-// récuperation De la class du bouton close
-const close = document.querySelector(".close")
-// récuperation de la class du container ou le modal ce situe 
+/* DOM Element - fermer Modal*/
+const closebtn = document.querySelector(".close")
 const bground = document.querySelector(".bground")
-// création de l'évènement pour changer le display: block en display:none
-close.addEventListener("click", ()=>{
+/* Event - fermer Modal*/
+closebtn.addEventListener("click", () => {
     bground.style.display = "none"
+    delMsgError()
 })
 
+/* Gestion Formulaire Inscription */
+// Etape1 récuperation DOM
+// 
+// Elements Globale formulaire
+const form = document.querySelector("form").tagName
+const checkContainer = document.querySelector(".check-container")
+const cuContainer = document.querySelector(".CU-container")
+const btnSubmit = document.querySelector(".btn-submit")
+// Elements donnees formulaire 
+const frist = document.querySelector("#frist")
+const last = document.querySelector("#last")
+const email = document.querySelector("#email")
+const birthdate = document.querySelector("#birthdate")
+const quantity = document.querySelector("#quantity")
+const citys = document.querySelector('#location6');
+const CU = document.querySelector('#checkbox1');
+const radioCheck = document.querySelector('input[name = "location"]:checked');
+// 
+//Etape2 Traitement données
+// 
+// Firstname / lastname fonction
+// 
+function ValidFirstName() {
+    if (!first.value) {
+        getError(first, "Veuillez renseigner un Prénom valide.")
+        // console.log("Veuillez renseigner un Prénom valide.");
+    } else if (first.value.length < 2) {
+        getError(first, "Le champs doit comporter minimum 2 caractères.")
+    } else {
+        return true
+    }
+}
+function ValidlastName() {
+    if (!last.value) {
+        getError(last, "Veuillez renseigner un Nom valide.")
+        // console.log("Veuillez renseigner un Prénom valide.");
+    } else if (last.value.length < 2) {
+        getError(last, "Le champs doit comporter minimum 2 caractères.")
+    } else {
+        return true
+    }
+}
+// 
+// Email fonction
+// 
+function ValidEmail() {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (!email.value) {
+        getError(email, "Veuillez renseigner un Email valide.")
+    } else if (!email.value.match(regex)) {
+        getError(email, "Format Email invalide (ex: test@test.com)")
+    } else {
+        return true
+    }
+}
+// 
+// BirthDate fonction
+// 
+function ValidBirthdate() {
+    if (!birthdate.value) {
+        getError(birthdate, "Veuillez entrer votre date de naissance.");
+        return false;
+    } else {
+        return true;
+    }
+}
+// 
+// CU fonction
+// 
+function ValidationCu() {
+    if (!CU.checked) {
+        getErrorCheckboxCU("Veuillez cocher la case \"conditions d'utilisation\".");
+    } else {
+        return true
+    }
+}
+// 
+// quantité fonction
+// 
+function validationQuantity() {
+    if (!quantity.value) {
+        getError(quantity, "Veuillez renseigner a combien de tournois avez-vous déjà participé.")
+        return false
+    } else {
+        return true
+    }
+}
+// 
+// localisation fonction
+// 
+function validLocation() {
+    if (radioCheck != null) {
+        //   setValidCheckbox(citys);
+        return true;
+    } else {
+        getErrorCheckbox("Veuillez renseigner une localisation.");
+        return false;
+    }
+}
+//   
+// Etape3 Gestion Erreur
+// 
+function getError(input, message) {
+    const createErrorMsg = document.createElement('p')
 
+    createErrorMsg.className = "error-message"
+    createErrorMsg.textContent = message
+    input.after(createErrorMsg)
+}
+function getErrorCheckbox(message) {
+    const createErrorMsg = document.createElement('p')
 
+    createErrorMsg.className = "error-message"
+    createErrorMsg.textContent = message
+    checkContainer.after(createErrorMsg)
+}
+function getErrorCheckboxCU(message) {
+    const createErrorMsg = document.createElement('p')
+
+    createErrorMsg.className = "error-message"
+    createErrorMsg.textContent = message
+    cuContainer.after(createErrorMsg)
+}
+// Suppression message-erreur
+
+function delMsgError() {
+    const errorMsg = document.querySelectorAll('.error-message')
+    for (let i = 0; i < errorMsg.length; i++) {
+        errorMsg[i].remove()
+    }
+}
+
+// 
+// Etape5 Validation Formulaire
+// 
+function validate(e) {
+    e.preventDefault();
+
+    ValidFirstName()
+    ValidlastName()
+    ValidEmail()
+    ValidBirthdate()
+    validationQuantity()
+    ValidationCu()
+    validLocation()
+}
